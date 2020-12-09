5480457e8fb29ef675b96452f635521fd186ff8a
xaviz5 <xavier5-20_lenin@hotmail.com>
2020-12-09 14:02:22 -0500

    En la clase RestEstudiante se modificó para usar el metodo insertarEstudiante de la clase ServicioEstudiante e ingresar los datos a la BDD desde postman

--- src/main/java/com/clearminds/lco/servicios/RestEstudiantes.java
+++ src/main/java/com/clearminds/lco/servicios/RestEstudiantes.java
@@ -6,6 +6,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 
 import com.clearminds.lco.dtos.Estudiante;
+import com.clearminds.lco.excepciones.BDDException;
 
 @Path("/estudiantes")
 public class RestEstudiantes {
@@ -14,7 +15,13 @@ public class RestEstudiantes {
 	@POST
 	@Consumes(MediaType.APPLICATION_JSON)
 	public void insertar(Estudiante estudiante){
-		System.out.println(estudiante);
+		ServicioEstudiante se = new ServicioEstudiante();
+		try {
+			System.out.println(estudiante);
+			se.insertarEstudiante(estudiante);
+		} catch (BDDException e) {
+			e.printStackTrace();
+		}
 	}
 	
 }
